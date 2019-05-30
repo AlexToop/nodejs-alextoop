75bd2da79d5be045495e09bfa75752d25814e4f2
Alex Toop <alextoop@outlook.com>
2019-05-30 16:44:48 +0100

    Express fix reference for public directory.

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ var express = require('express')
 ;
 
 module.exports.create = function (server, host, port, publicDir) {
-    var app = express()
-    ;
+    var app = express();
 
-    app.use(express.static(publicDir));
+    // app.use(express.static(publicDir));
+    app.use(express.static(path.join(__dirname, 'public')));
 
     return app;
 };
