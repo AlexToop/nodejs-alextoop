78f6b8ff1bc9a422306d472980ad92ed6b2497ad
Alex Toop <alextoop@outlook.com>
2019-05-30 14:48:23 +0100

    More SSL additions for HTTPS.

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var fs = require('fs');
+
+var PRIVATEKEY_PATH = path.join(__dirname, '../../../apache2/conf/server.key');
+var CERTIFICATE_PATH = path.join(__dirname, '../../../apache2/conf/server.crt');
+
+var privateKey = fs.readFileSync(PRIVATEKEY_PATH, 'utf8');
+var certificate = fs.readFileSync(CERTIFICATE_PATH, 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(PORT, HOST);
+httpsServer.listen(443, HOST);
 
 module.exports = app;
