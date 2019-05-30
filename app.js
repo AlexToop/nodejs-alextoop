2c8d96e27ce4bf8499501cdb9feeccb427915824
Alex Toop <alextoop@outlook.com>
2019-05-30 17:56:28 +0100

    Hide file extensions - Revert.

--- app.js
+++ app.js
@@ -29,9 +29,7 @@ var express = require('express');
 module.exports.create = function (server, host, port, publicDir) {
     var app = express();
 
-    app.use(express.static(publicDir), {
-        extensions: ['html']
-    });
+    app.use(express.static(publicDir));
     // app.use(express.static(path.join(__dirname, '..', 'public')));
 
     return app;
