91e412b85d5671eaab04307744aae1f16be9322b
Alex Toop <alextoop@outlook.com>
2019-05-30 16:33:15 +0100

    HTTPS support v2.

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+//
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+//
+// var app = express();
+//
+//
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+//
+//
+// module.exports = app;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+'use strict';
 
+var express = require('express')
+;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+module.exports.create = function (server, host, port, publicDir) {
+    var app = express()
+    ;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    app.use(express.static(publicDir));
 
-
-module.exports = app;
+    return app;
+};
