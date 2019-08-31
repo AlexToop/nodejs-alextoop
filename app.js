d5f61c444b5fa7efc28f9e6bf6c5555d0cb12521
Alex Toop <alextoop@outlook.com>
2019-08-31 18:39:29 +0100

    allowed cors

--- app.js
+++ app.js
@@ -32,5 +32,13 @@ module.exports.create = function (server, host, port, publicDir) {
     app.use(express.static(publicDir));
     // app.use(express.static(path.join(__dirname, '..', 'public')));
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "https://www.alextoop.com:8443");
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        next();
+    });
+
     return app;
 };
