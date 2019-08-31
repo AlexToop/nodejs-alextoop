699dca0885d41eb98f28570cc76c688ce02bac05
AlexToop <alextoop@outlook.com>
2019-08-31 17:12:58 +0100

    Cors policy added

--- bin/www.js
+++ bin/www.js
@@ -161,6 +161,28 @@ if (IS_PRODUCTION) {
         }
         var publicDir = path.join(__dirname, '..', 'public');
         var app = require('../app').create(server, host, port, publicDir);
+
+
+  
+        app.use(function (req, res, next) {
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', 'http://www.alextoop.com:8443/tasks');
+
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', true);
+
+            // Pass to next layer of middleware
+            next();
+        });
+
+
         listen(app);
     });
 } else {
