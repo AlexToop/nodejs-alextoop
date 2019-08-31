c08858b0a7bdbc7d427d9878efaeeb2c9698cc5d
Alex Toop <alextoop@outlook.com>
2019-08-31 18:42:47 +0100

    cors 3 attempt

--- app.js
+++ app.js
@@ -29,16 +29,14 @@ var express = require('express');
 module.exports.create = function (server, host, port, publicDir) {
     var app = express();
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+
     app.use(express.static(publicDir));
     // app.use(express.static(path.join(__dirname, '..', 'public')));
-
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "https://www.alextoop.com:8443/tasks");
-        res.header("Access-Control-Allow-Credentials", "true");
-        res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        next();
-    });
 
     return app;
 };
