2b08235383a4718ef4459eb480816eb2414eccd9
AlexToop <alextoop@outlook.com>
2019-09-01 09:30:45 +0100

    testing new cors edit

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ var express = require('express');
 
 module.exports.create = function (server, host, port, publicDir) {
     var app = express();
+    var cors = require('cors');
 
+    app.use(cors());
     app.use(express.static(publicDir));
     // app.use(express.static(path.join(__dirname, '..', 'public')));
 
